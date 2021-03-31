3a389e12e271c2732ff9826d1afe16baa56d4b86
bernipsictra <walhalla01wal@gmail.com>
2021-03-31 20:03:01 +0800

    First Android Project

---
+++ app/src/main/java/com/example/myapplication/lesson03/Imageview.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.lesson03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Imageview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imageview);
+    }
+}
