d40801c940a52b708021373d39a9b4ad5a956cbf
bernipsictra <walhalla01wal@gmail.com>
2021-04-01 11:45:43 +0800

    First Android Project

--- app/src/main/java/com/example/myapplication/lesson03/Spinnerlab.java
+++ app/src/main/java/com/example/myapplication/lesson03/Spinnerlab.java
@@ -3,20 +3,14 @@ package com.example.myapplication.lesson03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import com.example.myapplication.R;
 
-public class Imageview extends AppCompatActivity {
-    private ImageView iv;
+public class Spinnerlab extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_imageview);
-
-        iv = (ImageView) findViewById(R.id.iv);
-        iv.setImageResource(R.drawable.mountn);
-
+        setContentView(R.layout.lesson03_spinnerlab);
     }
 }
